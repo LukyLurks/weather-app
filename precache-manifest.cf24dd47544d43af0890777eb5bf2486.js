116ef13f6dfd57956c1f7610b83adbfe03152527
Luckner Cyné <luckner@cyne.fr>
2020-06-06 11:09:02 +0200

    Updates

---
+++ precache-manifest.cf24dd47544d43af0890777eb5bf2486.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48461cac0763b26145e30270537a0fbc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7f629c757e8d1dff1ec0",
+    "url": "/weather-app/static/css/main.ef6ce4b4.chunk.css"
+  },
+  {
+    "revision": "782567b6bbfbc8f4753d",
+    "url": "/weather-app/static/js/2.ffdc2674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.ffdc2674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f629c757e8d1dff1ec0",
+    "url": "/weather-app/static/js/main.f471e849.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "f752961f8238db21576017a030e37b0e",
+    "url": "/weather-app/static/media/clear-alexander-mils-unsplash-small.f752961f.jpg"
+  },
+  {
+    "revision": "017d20493d06b20208ed09088b31685c",
+    "url": "/weather-app/static/media/clouds-drew-hays-unsplash-small.017d2049.jpg"
+  },
+  {
+    "revision": "e4270e9eaebc54b9abfd32d1e5978c90",
+    "url": "/weather-app/static/media/drizzle-anant-chandra-unsplash-small.e4270e9e.jpg"
+  },
+  {
+    "revision": "8a3ff825191bf2e1228de470c639c7e4",
+    "url": "/weather-app/static/media/obscured-justin-cron-unsplash-small.8a3ff825.jpg"
+  },
+  {
+    "revision": "51fdba3251bc81ace0eea9399298a8ed",
+    "url": "/weather-app/static/media/rain-lucas-fernando-unsplash-small.51fdba32.jpg"
+  },
+  {
+    "revision": "db26475123a2675e7e7fe79bdca8acc8",
+    "url": "/weather-app/static/media/snow-chandler-cruttenden-unsplash-small.db264751.jpg"
+  },
+  {
+    "revision": "46abc5c87e2d9683f2f2308ed19d3173",
+    "url": "/weather-app/static/media/thunderstorm-jonas-kaiser-unsplash-small.46abc5c8.jpg"
+  }
+]);
